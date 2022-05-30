32a2961284bac472960cf3d32d467b3a84428498
dswagh <dswagh9@gmail.com>
2022-05-30 13:10:45 +0530

    Updates

---
+++ precache-manifest.87a0aa86bf20d33522c6e77681ee827f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "78b6179a3aa97b891635",
+    "url": "/textModifier/static/css/main.5022869c.chunk.css"
+  },
+  {
+    "revision": "78b6179a3aa97b891635",
+    "url": "/textModifier/static/js/main.78b6179a.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/textModifier/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "b649ee8760ab0a60bd8e",
+    "url": "/textModifier/static/css/2.e56b036b.chunk.css"
+  },
+  {
+    "revision": "b649ee8760ab0a60bd8e",
+    "url": "/textModifier/static/js/2.b649ee87.chunk.js"
+  },
+  {
+    "revision": "6b99433f800e4d9bcbf8",
+    "url": "/textModifier/static/js/runtime~main.6b99433f.js"
+  },
+  {
+    "revision": "c60c4e801eee0f4e5f606a52a5486128",
+    "url": "/textModifier/index.html"
+  }
+];
